7c209b5a079cdc2d63cbb1bb67f3b09f654714df
Adolfo Ochagavía <aochagavia92@gmail.com>
2016-12-08 12:46:39 +0100

    RangeInt becomes a list!

--- src/Range.hs
+++ src/Range.hs
@@ -6,16 +6,14 @@ data Range = RangeInt IntRange
            | RangeBool BoolRange
     deriving (Show)
 
-data IntRange = InclusiveInclusive MyInt MyInt
-              | Disjoint IntRange IntRange
-    deriving (Show)
-
 data MyInt = Bounded Int
            | MinInfinite
            | MaxInfinite
     deriving (Eq, Show)
 
 type BoolRange = Set.Set Bool
+type IntRange = [Interval]
+type Interval = (MyInt, MyInt)
 
 instance Ord MyInt where
     compare MinInfinite MinInfinite = EQ
@@ -31,17 +29,15 @@ rFalse = RangeBool $ Set.singleton False
 rTrue = RangeBool $ Set.singleton True
 
 leftInfinite, rightInfinite :: Int -> Range
-leftInfinite n = RangeInt $ InclusiveInclusive MinInfinite $ Bounded n
-rightInfinite n = RangeInt $ InclusiveInclusive (Bounded n) MaxInfinite
+leftInfinite n = RangeInt $ [(MinInfinite, Bounded n)]
+rightInfinite n = RangeInt $ [(Bounded n, MaxInfinite)]
 
 bounded :: Int -> Int -> Range
-bounded low up = RangeInt $ InclusiveInclusive (Bounded low) (Bounded up)
+bounded low up = RangeInt $ [(Bounded low, Bounded up)]
 
 unionRange :: Range -> Range -> Range
 unionRange (RangeBool b1) (RangeBool b2) = RangeBool $ Set.union b1 b2
-unionRange (RangeInt r1) (RangeInt r2) = RangeInt $ unionRangeInt r1 r2
-    where unionRangeInt :: IntRange -> IntRange -> IntRange
-          unionRangeInt (InclusiveInclusive begin1 end1) (InclusiveInclusive begin2 end2) = undefined
+unionRange (RangeInt r1) (RangeInt r2) = RangeInt $ undefined
 
 unionRange _ _ = error "Called unionRange with incompatible ranges"
 
